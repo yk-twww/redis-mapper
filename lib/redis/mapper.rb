40ea522bed16e0bb485978f71b6382a75a15ec38
yk-twww <yk.twww@gmail.com>
2015-05-29 23:20:26 -0400

    First commit

---
+++ lib/redis/mapper.rb
@@ -0,0 +1,7 @@
+require "redis/mapper/version"
+
+module Redis
+  module Mapper
+    # Your code goes here...
+  end
+end
---
+++ spec/redis/mapper_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Redis::Mapper do
+  it 'has a version number' do
+    expect(Redis::Mapper::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
